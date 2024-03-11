64a413a576e6c30d57e49032cd921a8098c0da00
JHRK <102567282+JHRK@users.noreply.github.com>
2024-03-11 08:31:57 -0300

    Update main.php

--- INCLUDES/main.php
+++ INCLUDES/main.php
@@ -9,9 +9,9 @@
     <img  class="col-12" src="./ASSETS/gifboome.gif">   
     </div><div class="col-3 ola" id="toy"><hr><hr><hr><hr><hr><hr><hr></div>
     <div class="col ola"><h1><a href="./documentos.php">Documentação</a></h1></div>
-    <div class="col ola"><h1>Projeções</h1></div>
+    <div class="col ola"><h1>Impacto</h1></div>
     <div class="col ola">
-        <h1 >Componentes</h1>
+        <h1 >Partes do Projeto</h1>
     </div>
     <div class="col ola">
     <h1>Interativo</h1>
